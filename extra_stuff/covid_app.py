0ab8fac20663c71ad1c5d32cbe61adacac64eebc
krazy-kudu <66883099+krazy-kudu@users.noreply.github.com>
2020-09-19 17:54:08 -0700

    boom! done

--- extra_stuff/covid_app.py
+++ extra_stuff/covid_app.py
@@ -8,13 +8,14 @@ app = Flask(__name__)
 # Use PyMongo to establish Mongo connection
 mongo = PyMongo(app, uri="mongodb://localhost:27017/covid_app")
 
+#mongo.db.dropDatabase()
 
 # Route to render index.html template using data from Mongo
 @app.route("/")
 def home():
 
     # Find one record of data from the mongo database
-    displayed_data = mongo.db.collection.find_one()
+    displayed_data = mongo.db.collection.find()
 
     # Return template and data
     return render_template("index.html", sickly=displayed_data)
@@ -27,8 +28,9 @@ def scrape():
     # Run the scrape function
     covid_data = scraper.scrape_info()
 
+    print(covid_data)
     # Update the Mongo database using update and upsert=True
-    mongo.db.collection.update({}, covid_data, upsert=True)
+    mongo.db.collection.insert_many(covid_data)
 
     # Redirect back to home page
     return redirect(url_for('home'))
